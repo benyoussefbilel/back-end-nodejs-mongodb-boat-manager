1f60c7d893f47461532e2f5e8ea6a07841cf2a07
benyoussefbilel <benyoussefbilel4@gmail.com>
2020-11-16 15:50:06 +0100

    securing with env-cmd

--- src/models/user.js
+++ src/models/user.js
@@ -88,7 +88,7 @@ userSchema.methods.toJSON = function(){
 // generatingAuthtoken
 userSchema.methods.generateAuthtoken = async function(){
     const user = this
-    const token = jwt.sign({ _id: user._id.toString()},'medisail')
+    const token = jwt.sign({ _id: user._id.toString()},process.env.JWT_SECRET)
     user.tokens = user.tokens.concat({token})
     await user.save()
     return token
